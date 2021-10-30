67a4eeb470db5703bf59ec38aa8f2e8e1b51e461
praveenasajja <92560801+praveenasajja@users.noreply.github.com>
2021-10-29 22:13:16 -0400

    traning 2 notes and code changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App1 = (props) => {
+  return <div>{props.childText}</div>;
+};
+const App2 = (props) => {
+  return <div>{props.child2Text}</div>;
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      childText: "this is stateless componet",
+      parentText: "this is class based component",
+      child2Text: "this is child2 stateless component",
+    };
+  }
+  changeChild2State = () => {
+    this.setState({
+      child2Text: "i am changed",
+    });
+  };
+  render() {
+    return (
+      <div>
+        <div>{this.state.parentText}</div>
+        <button onClick={this.changeChild2State}>Modify Child2 text</button>
+        <App1 childText={this.state.childText} />
+        <App2 child2Text={this.state.child2Text} />
+      </div>
+    );
+  }
 }
-
 export default App;
